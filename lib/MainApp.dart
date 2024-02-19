a478b39d9b40251df3c1012d8637a14d99e071d1
joel <joelthousend@gmail.com>
2024-02-19 18:26:35 +0300

    doing the ui part

---
+++ lib/MainApp.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Row(
+          children: [
+            const Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 10.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.black)),
+                      hintStyle: TextStyle(color: Colors.grey),
+                      hintText: "Enter A City"),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              child: const ElevatedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                    padding: MaterialStatePropertyAll(EdgeInsets.all(20.0))),
+                child: Icon(Icons.sunny),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/MainApp.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final String uri =
+      "http://api.weatherapi.com/v1/current.json?key=f6aedda0032042e1812150300231109&q=London&aqi=no";
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text("Weather"),
+          Container(
+              margin: const EdgeInsets.only(left: 10.0),
+              child: const Icon(Icons.sunny))
+        ]),
+      ),
+      body: const MainApp(),
+    ));
   }
 }
