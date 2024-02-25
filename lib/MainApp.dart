34e616be5d74cf1c67326ad2caddaf35a75f38b2
joel <joelthousend@gmail.com>
2024-02-25 16:41:27 +0300

    finished it

--- lib/MainApp.dart
+++ lib/MainApp.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_weather_app/SearchAndFilter.dart';
 import 'package:flutter_weather_app/weather_api.dart';
+import "package:http/http.dart" as http;
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -12,15 +16,12 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  String query = "Addis Abeba";
+  String query = "Addis Ababa";
 
   TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(
-        Duration.zero, () => showDialog(context: context, builder: makeDialog));
-
     return ListView(
       children: [
         SearchAndFilter(
@@ -29,11 +30,46 @@ class _MainAppState extends State<MainApp> {
         ),
         Container(
           alignment: Alignment.center,
-          child: FutureBuilder(
-              future: getResponse(query, context), builder: buildTheCard),
+          child:
+              FutureBuilder(future: getResponse(query), builder: buildTheCard),
         )
       ],
     );
+  }
+
+  Future<CityInfo?> getResponse(String query) async {
+    const String apiKey = "f6aedda0032042e1812150300231109";
+
+    var uri = Uri.http("api.weatherapi.com", "/v1/current.json", {"q": query});
+
+    final response = await http.get(uri, headers: {"key": apiKey});
+
+    if (context.mounted && Navigator.canPop(context)) {
+      Navigator.pop(context);
+    }
+
+    if (response.statusCode == 200) {
+      var responseJson = jsonDecode(response.body);
+
+      String cityName = responseJson["location"]["name"].toString();
+      String temperature =
+          "${responseJson["current"]["temp_c"]}\u2103".toString();
+      String weatherIcon =
+          responseJson["current"]["condition"]["icon"].toString();
+      String weatherCondition =
+          responseJson["current"]["condition"]["text"].toString();
+      String windSpeedInKph = responseJson["current"]["wind_kph"].toString();
+      String humidity = responseJson["current"]["humidity"].toString();
+
+      return CityInfo(cityName, temperature, weatherIcon, weatherCondition,
+          windSpeedInKph, humidity);
+    } else {
+      if (kDebugMode) {
+        print("my data: ${response.statusCode}");
+      }
+    }
+
+    return null;
   }
 
   Widget buildTheCard(BuildContext context, AsyncSnapshot<CityInfo?> snapshot) {
@@ -63,6 +99,7 @@ class _MainAppState extends State<MainApp> {
   }
 
   void searchClicked() {
+    showDialog(context: context, builder: makeDialog);
     String getQuery = textEditingController.text;
     setState(() {
       query = getQuery;
--- lib/weather_api.dart
+++ lib/weather_api.dart
@@ -1,16 +1,10 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import "package:http/http.dart" as http;
-import 'package:flutter/foundation.dart';
-
 class CityInfo {
-  String? _cityName;
-  String? _temperature;
-  String? _weatherCondition;
-  String? _windSpeedInKph;
-  String? _humidity;
-  String? _icon;
+  final String? _cityName;
+  final String? _temperature;
+  final String? _weatherCondition;
+  final String? _windSpeedInKph;
+  final String? _humidity;
+  final String? _icon;
 
   CityInfo(this._cityName, this._temperature, this._icon,
       this._weatherCondition, this._windSpeedInKph, this._humidity);
@@ -39,40 +33,4 @@ class CityInfo {
     String? modifiedUri = "http:$_icon";
     return modifiedUri;
   }
-}
-
-Future<CityInfo?> getResponse(String query, BuildContext context) async {
-  const String apiKey = "f6aedda0032042e1812150300231109";
-
-  var uri = Uri.http("api.weatherapi.com", "/v1/current.json", {"q": query});
-
-  final response = await http.get(uri, headers: {"key": apiKey});
-
-  if (context.mounted) {
-    Navigator.of(context).pop();
-  }
-
-  if (response.statusCode == 200) {
-    if (kDebugMode) {
-      var responseJson = jsonDecode(response.body);
-
-      String cityName = responseJson["location"]["name"];
-      String temperature = "${responseJson["current"]["temp_c"]}\u2103";
-      String weatherIcon = responseJson["current"]["condition"]["icon"];
-      String weatherCondition = responseJson["current"]["condition"]["text"];
-      String windSpeedInKph = responseJson["current"]["wind_kph"];
-      String humidity = responseJson["current"]["humidity"];
-
-      print("kph $windSpeedInKph");
-
-      return CityInfo(cityName, temperature, weatherIcon, weatherCondition,
-          windSpeedInKph, humidity);
-    }
-  } else {
-    if (kDebugMode) {
-      print("my data: ${response.statusCode}");
-    }
-  }
-
-  return null;
 }
